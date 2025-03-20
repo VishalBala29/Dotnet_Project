9e1b9324ce857e6b8717c08480b4e850886a6533
Vishal <125129686+VishalBala29@users.noreply.github.com>
2025-03-20 11:55:39 +0530

    Add files via upload

---
+++ C#_Project/Sample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Console.WriteLine("Hello " + Convert.ToString(num));
+
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum: " + (num + num1));
+
+            string str = Convert.ToString(num1);
+            Console.WriteLine("String version of input: " + str);
+            Console.WriteLine("Concatenation: " + num + str); // concatenating
+            Console.WriteLine("Square root: " + Math.Sqrt(num1));
+            Console.WriteLine("String length: " + str.Length);
+
+            // Using Sample class
+            Sample sample = new Sample();
+            int result = sample.Add(num, num1);
+            Console.WriteLine("Sum using class method: " + result);
+        }
+    }
+
+    class Sample
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
